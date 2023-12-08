67aa9ab95ff4de4cde582e935976a71ebf63b736
yaser <y.r.k.r.2011@gmail.com>
2023-12-08 19:51:57 +0100

    first

---
+++ Reinforce.java
@@ -0,0 +1,18 @@
+package reinforceproject;
+
+class Example {
+	int number;
+	String name;
+}
+
+public class Reinforce {
+
+	public static void main(String[] args) {
+		Example example = new Example();
+		example.number = 100;
+		example.name = "yaser";
+		System.out.println("my name is " + example.name + " and my number is " + example.number);
+
+	}
+
+}
