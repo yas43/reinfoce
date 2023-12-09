a13adc9617f2869fa3ddcbb8d872703012722fdd
yaser <y.r.k.r.2011@gmail.com>
2023-12-09 14:57:45 +0100

    second commitment

--- Reinforce.java
+++ Reinforce.java
@@ -1,18 +1,1 @@
-package reinforceproject;
-
-class Example {
-	int number;
-	String name;
-}
-
-public class Reinforce {
-
-	public static void main(String[] args) {
-		Example example = new Example();
-		example.number = 100;
-		example.name = "yaser";
-		System.out.println("my name is " + example.name + " and my number is " + example.number);
-
-	}
-
-}
+this is reinforce pro
